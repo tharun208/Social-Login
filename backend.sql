1780b0c679fb32862354a45ad407f60bb479879f
Ubuntu <ubuntu@ip-172-31-17-170.us-east-2.compute.internal>
2018-04-07 05:47:17 +0000

    added db dump file

--- backend.sql
+++ backend.sql
@@ -1,106 +1,104 @@
--- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
 --
--- Host: localhost
--- Generation Time: Apr 05, 2018 at 12:20 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.20
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: backend
+-- ------------------------------------------------------
+-- Server version	5.7.21-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `backend`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `facebookuser`
 --
 
+DROP TABLE IF EXISTS `facebookuser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `facebookuser` (
   `userid` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `facebookuser`
 --
 
-INSERT INTO `facebookuser` (`userid`, `username`) VALUES
-('2090545357847773', 'Tharun Rajendran');
-
--- --------------------------------------------------------
+LOCK TABLES `facebookuser` WRITE;
+/*!40000 ALTER TABLE `facebookuser` DISABLE KEYS */;
+INSERT INTO `facebookuser` VALUES ('2090545357847773','Tharun Rajendran');
+/*!40000 ALTER TABLE `facebookuser` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `googleuser`
 --
 
+DROP TABLE IF EXISTS `googleuser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `googleuser` (
   `userid` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `googleuser`
 --
 
-INSERT INTO `googleuser` (`userid`, `username`, `email`) VALUES
-('105565190919104206731', 'tharun rajendran', 'rajendrantharun@gmail.com');
-
--- --------------------------------------------------------
+LOCK TABLES `googleuser` WRITE;
+/*!40000 ALTER TABLE `googleuser` DISABLE KEYS */;
+INSERT INTO `googleuser` VALUES ('105565190919104206731','tharun rajendran','rajendrantharun@gmail.com');
+/*!40000 ALTER TABLE `googleuser` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `mobile` varchar(255) NOT NULL,
   `dob` varchar(255) NOT NULL,
-  `secretKey` varchar(255) NOT NULL
+  `secretKey` varchar(255) NOT NULL,
+  PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`name`, `email`, `password`, `mobile`, `dob`, `secretKey`) VALUES
-('Tharun R', 'rajendrantharun@live.com', 'lovecode<3', 'undefined', '20-08-1996', 'FAYVGJRVJFQTORJUPMQSCQBEPVUCY2KX');
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('a','a@gmail.com','a','undefined','20-08-1996',''),('Tharun','rajen@gmail.com','a','undefined','20-08-1996','I5GH2PTTOZWEEUTZJ5JG6MKNKNEE6NSP'),('Sathya Naraynan K','sathyanaren1210@gmail.com','Kumaravel21vky','undefined','12-OCT-1996','MZ4XKPS3HFDSYVB4FJIWOP25OQUDKUKC'),('Xxx','xxx@gmail.com','','undefined','14-2-1997',''),('Aaa','yyyy@gmail.com','12345','undefined','14-2-1997','');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `facebookuser`
---
-ALTER TABLE `facebookuser`
-  ADD PRIMARY KEY (`userid`);
-
---
--- Indexes for table `googleuser`
---
-ALTER TABLE `googleuser`
-  ADD PRIMARY KEY (`userid`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`email`);
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-07  5:45:12
